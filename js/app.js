5326b65a9a3a022fe384ea3d279b2419c5fcef39
Emmy Williams <emmy.kayanja@students.mak.ac.ug>
2023-08-18 06:29:16 +0300

    emmy williams

--- js/app.js
+++ js/app.js
@@ -7,6 +7,30 @@ $(function () {
   new Window().innerWidth();
 });
 
+// JavaScript to close the navbar when clicking a link
+function closeNavbar() {
+  const navBar = document.querySelector(".navbar-collapse");
+  if (navBar.classList.contains("show")) {
+    navBar.classList.remove("show");
+  }
+
+  if (window.innerWidth <= 992) {
+    let navbar = document.querySelector(".navbar-collapse");
+    if (navbar.classList.contains("show")) {
+      navbar.classList.remove("show");
+    }
+  }
+}
+
+// function closeNavbar() {
+//   if (window.innerWidth <= 992) {
+//     let navbar = document.querySelector(".navbar-collapse");
+//     if (navbar.classList.contains("show")) {
+//       navbar.classList.remove("show");
+//     }
+//   }
+// }
+
 // $(document).ready(function () {
 //   // Hero slider
 //   $("#hero-slider").owlCarousel({
